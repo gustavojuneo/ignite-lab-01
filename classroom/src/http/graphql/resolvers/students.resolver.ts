939b14ef61e4fbd3d352affc524698f164cf9c2d
Gustavo Santos <gustavojuneo@outlook.com>
2022-05-02 18:26:40 -0300

    Aula 02 - Resolver & Queries

---
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -0,0 +1,34 @@
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query, ResolveField, Parent } from '@nestjs/graphql';
+
+import { AuthorizationGuard } from '../../auth/authorization.guard';
+import { Student } from '../models/student';
+
+import { StudentsService } from '../../../services/students.service';
+import { EnrollmentsService } from '../../../services/enrollments.service';
+import { AuthUser, CurrentUser } from '../../auth/current-user';
+
+@Resolver(() => Student)
+export class StudentsResolver {
+  constructor(
+    private studentsService: StudentsService,
+    private enrollmentsService: EnrollmentsService,
+  ) {}
+
+  @Query(() => Student)
+  @UseGuards(AuthorizationGuard)
+  me(@CurrentUser() user: AuthUser) {
+    return this.studentsService.getStudentByAuthUserId(user.sub);
+  }
+
+  @Query(() => [Student])
+  @UseGuards(AuthorizationGuard)
+  students() {
+    return this.studentsService.listAllStudents();
+  }
+
+  @ResolveField()
+  enrollments(@Parent() student: Student) {
+    return this.enrollmentsService.listEnrollmentsByStudentId(student.id);
+  }
+}
